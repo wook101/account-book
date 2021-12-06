df388f4022df6a5fe653b8aabf7b3c6504062906
wook101 <ehddnr1022@gmail.com>
2021-12-06 23:47:01 +0900

    AccountBookDto 수정

---
+++ src/main/java/com/payhere/app/dto/AccountBookDto.java
@@ -0,0 +1,11 @@
+package com.payhere.app.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class AccountBookDto {
+    private int price;
+    private String memo;
+    private String category;
+}
