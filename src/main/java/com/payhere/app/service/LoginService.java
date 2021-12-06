c4af162a630ad520c9cdccf09b8dac48d0633a2c
wook101 <ehddnr1022@gmail.com>
2021-12-06 20:06:49 +0900

    LoginService login구현

--- src/main/java/com/payhere/app/service/LoginService.java
+++ src/main/java/com/payhere/app/service/LoginService.java
@@ -1,18 +1,24 @@
 package com.payhere.app.service;
 
-import com.payhere.app.jwt.TokenProvider;
+import com.payhere.app.dao.LoginDao;
+import com.payhere.app.domain.User;
+import com.payhere.app.dto.UserDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
 
-    private TokenProvider tokenProvider;
+    private final LoginDao loginDao;
 
-    public LoginService(TokenProvider tokenProvider){
-        this.tokenProvider = tokenProvider;
+    public LoginService(LoginDao loginDao){
+        this.loginDao = loginDao;
     }
 
-
+    public boolean login(UserDto userDto){
+        User user = loginDao.findByEmailAndPassword(userDto);
+        if (user==null) return false;
+        return true;
+    }
 
 
 }
